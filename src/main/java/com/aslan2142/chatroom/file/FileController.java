3d740749ab5b41681912bf5fa8b8cd186365d659
Aslan2142 <Aslan2142@gmail.com>
2020-12-11 20:33:01 -0500

    Fixed video attachments

--- src/main/java/com/aslan2142/chatroom/file/FileController.java
+++ src/main/java/com/aslan2142/chatroom/file/FileController.java
@@ -3,6 +3,7 @@ package com.aslan2142.chatroom.file;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -24,7 +25,7 @@ public class FileController {
     {
         Resource file = fileService.serveFile(filename);
         
-        return ResponseEntity.ok().header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + file.getFilename() + "\"").body(file);
+        return ResponseEntity.ok().contentType(MediaType.APPLICATION_OCTET_STREAM).header(HttpHeaders.CONTENT_DISPOSITION, "filename=\"" + file.getFilename() + "\"").body(file);
     }
 
     @PostMapping(path = "{filename}")
